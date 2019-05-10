a56658da3b30c401e6e6fd2bf0ab74d80c5a6a28
Morgan Wessel <mhwessel3@gmail.com>
2019-05-10 13:12:46 +0200

    completed the class structure

---
+++ WordPredictTrainer.py
@@ -0,0 +1,5 @@
+def main():
+    print("Trainer")
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+
+def main():
+    """
+    Main method. Decodes command-line arguments, and starts the Word Prediction Trainer and/or Tester.
+    """
+    try:
+        f = open('/Users/mhwessel3/Desktop/WordPrediction/', 'r')
+        # Store configuration file values
+    except FileNotFoundError:
+        # Keep preset values
+        print("File not found.")
+
+if __name__ == '__main__':
+    main()
