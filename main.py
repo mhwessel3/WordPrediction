823dbab3ddbddebb0ec9c0c9501ff93b92f016c5
Morgan Wessel <mhwessel3@gmail.com>
2019-05-10 16:23:08 +0200

    set up the gui

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
 import sys
+import curses
+import os
 
-def main():
+def main(win):
     """
     Main method. Decodes command-line arguments, and starts the Word Prediction Trainer and/or Tester.
     """
-    try:
-        f = open('/Users/mhwessel3/Desktop/WordPrediction/', 'r')
-        # Store configuration file values
-    except FileNotFoundError:
-        # Keep preset values
-        print("File not found.")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+    print("- - - - - - - - - - - - WORD PREDICTOR - - - - - - - - - - - - ")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+    print("""\
+                                       ._ o o
+                                       \_`-)|_
+                                    ,""       \ 
+                                  ,"  ## |   ಠ ಠ. 
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)
+                            ,"     ## /
+                          ,"   ##    /
+                    """)
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+    print("- - - - ENTER YOUR TEXT BELOW, ONE CHARACTER AT A TIME - - - - ")
+    print("- - - - - TO ESCAPE THE PROGRAM, TYPE \"CTRL C\" - - - - - - - ")
 
-if __name__ == '__main__':
-    main()
+    win.nodelay(True)
+    key=""
+    win.clear()
+    win.addstr("word1 word2 word3\n")              
+    win.addstr("")
+
+    curr_str = ""
+    while True:
+        try:
+           key = win.getkey()
+           curr_str += key
+           win.clear()
+           win.addstr("word1 word2 word3\n")
+           win.addstr(curr_str)
+           
+           if key == os.linesep:
+              break
+        except Exception as e:
+           # No input
+           pass
+        count += 1    
+
+curses.wrapper(main)
