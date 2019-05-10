5f13c8e7a124bd42b014d6b1724b44d564a779d9
Morgan Wessel <mhwessel3@gmail.com>
2019-05-10 21:04:38 +0200

    edited the direction prompt and main window

--- main.py
+++ main.py
@@ -2,6 +2,39 @@ import sys
 import curses
 import os
 
+def start_program(win):
+    win.nodelay(True)
+    key=""
+    win.clear()
+    win.addstr("word1 word2 word3\n")              
+    win.addstr("")
+
+    curr_str = ""
+    while True:
+        try:
+           key = win.getkey()
+           curr_str += key
+           win.clear()
+
+           # call helper function here
+           suggestions = getSuggestions()
+           win.addstr(suggestions)
+           win.addstr(curr_str)
+           
+           if key == os.linesep:
+              break
+        except Exception as e:
+           # No input
+           pass
+    
+
+
+def getSuggestions():
+    """
+    Returns the top three word suggestions that the user may want to type
+    """
+    return "word1 word2 word3\n"
+
 def main(win):
     """
     Main method. Decodes command-line arguments, and starts the Word Prediction Trainer and/or Tester.
@@ -20,29 +53,13 @@ def main(win):
                           ,"   ##    /
                     """)
     print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
-    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
-    print("- - - - ENTER YOUR TEXT BELOW, ONE CHARACTER AT A TIME - - - - ")
+    print("- - - AS YOU ENTER KEYS, POTENTIAL WORDS WILL POPULATE - - - - ")
     print("- - - - - TO ESCAPE THE PROGRAM, TYPE \"CTRL C\" - - - - - - - ")
+    print("- - - - - - - - - PRESS <enter> TO BEGIN - - - - - - - - - - - ")
+    
+    # START THE PROGRAM
+    input()
+    curses.wrapper(start_program)
 
-    win.nodelay(True)
-    key=""
-    win.clear()
-    win.addstr("word1 word2 word3\n")              
-    win.addstr("")
-
-    curr_str = ""
-    while True:
-        try:
-           key = win.getkey()
-           curr_str += key
-           win.clear()
-           win.addstr("word1 word2 word3\n")
-           win.addstr(curr_str)
-           
-           if key == os.linesep:
-              break
-        except Exception as e:
-           # No input
-           pass
-
-curses.wrapper(main)
+if __name__ == "__main__":
+    main("heh")
